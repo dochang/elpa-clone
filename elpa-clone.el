ec6ca2a07d4aea7abc38e21ef234f97b9ed97ce3
Wade Zhang <dochang@gmail.com>
2021-09-14 11:57:24 +0800

    Disable checking dead link for saythanks.io
    But do not remove the badge for now.  The project seems being maintained
    actively.

--- elpa-clone.el
+++ elpa-clone.el
@@ -27,12 +27,20 @@
 
 ;;; Commentary:
 
+;; <!-- markdown-link-check-disable -->
 ;; [![MELPA](http://melpa.org/packages/elpa-clone-badge.svg)](http://melpa.org/#/elpa-clone)
 ;; [![MELPA Stable](http://stable.melpa.org/packages/elpa-clone-badge.svg)](http://stable.melpa.org/#/elpa-clone)
 ;; [![Build Status](https://cloud.drone.io/api/badges/dochang/elpa-clone/status.svg)](https://cloud.drone.io/dochang/elpa-clone)
 ;; [![Build Status](https://travis-ci.org/dochang/elpa-clone.svg?branch=master)](https://travis-ci.org/dochang/elpa-clone)
 ;; [![GitHub](https://img.shields.io/github/license/dochang/elpa-clone)](https://github.com/dochang/elpa-clone/blob/master/LICENSE)
 ;; [![Say Thanks!](https://img.shields.io/badge/say-thanks-green)](https://saythanks.io/to/dochang)
+;; <!--
+;; See the following issues for details.
+;;
+;; <https://github.com/BlitzKraft/saythanks.io/issues/60>
+;; <https://github.com/BlitzKraft/saythanks.io/issues/103>
+;; -->
+;; <!-- markdown-link-check-enable -->
 ;;
 ;; Mirror an ELPA archive into a directory.
 
